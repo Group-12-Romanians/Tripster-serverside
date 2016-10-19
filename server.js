926c6ab4db6ee524d56c5b01f5de29cb09903072
dragos <dii14@ic.ac.uk>
2016-10-19 14:33:00 +0100

    testing hooks

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require("http");
 var port = process.env.PORT || 8081
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello World! We are group 12 and this app is running!\n');
+	response.end('Hello World! We are group 12 and this app is running!!!\n');
 }).listen(port);
 
 // Console will print the message
