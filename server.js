ac93ca26a7f0aca84025b37aa8413b382dcab93c
Cristian Chirac <cristian.chirac14@imperial.ac.uk>
2016-10-27 17:14:27 +0100

    Implemented basic app get

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var http = require("http");
-var port = process.env.PORT || 8081
-http.createServer(function (request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello World! We are group 12 and this app is running!!!\n');
-}).listen(port);
+var express = require('express');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var app = express();
 
-// Console will print the message
-console.log('Server running on port: ' + port);
+var port = process.env.PORT || 8081
+
+app.use(express.static('public'));
+
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.get('/process_get', function(req, res) {
+    var request = url.parse(req.url, true);
+    var action = request.pathname;
+    var filePath = path.join(__dirname, action).split('%20').join(' ');
+    var ext = path.extname(action);
+    console.log(ext);
+})
+
+app.listen(port);
+
+console.log('Server running now on port: ' + port);
 
