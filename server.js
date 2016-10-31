3bb89b1e0c6d977c8aa151f396e59a8eb0794110
dragos <dii14@ic.ac.uk>
2016-10-31 21:07:55 +0000

    Testing locations syncing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var url = require('url');
 var path = require('path');
@@ -7,10 +8,12 @@ var app = express();
 var port = process.env.PORT || 8081
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
-})
+});
 
 app.get('/process_get', function(req, res) {
     var request = url.parse(req.url, true);
@@ -18,7 +21,17 @@ app.get('/process_get', function(req, res) {
     var filePath = path.join(__dirname, action).split('%20').join(' ');
     var ext = path.extname(action);
     console.log(ext);
-})
+});
+
+app.post('/sync_locations', function(req, res) {
+	var locations = req.body.locations;
+	console.log("locations" + locations);
+	if (locations === "Dragos") {
+		res.send("OK!");
+	} else {
+		res.send("NOTOK!");
+	}
+});
 
 app.listen(port);
 
