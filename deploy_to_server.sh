9345945e3ac5e17f981d7798e23c0cc2d2667551
dragos <dii14@ic.ac.uk>
2016-11-08 18:52:00 +0000

    testing deployment method

--- deploy_to_server.sh
+++ deploy_to_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ssh -q dii14@shell3.doc.ic.ac.uk 'rm -rf ~/.tmpTripster/*'
-ssh -q dii14@shell3.doc.ic.ac.uk 'ssh -q dii14@146.169.46.220 "'"rm -rf ~/app"'"'
-echo 'cleaned ~/.tmpTripster on shell3 and app on 146.169.46.220'
+ssh -q dii14@shell3.doc.ic.ac.uk 'ssh -q dii14@146.169.46.220 "'"cd app && rm -rf !(node_modules)"'"'
+echo 'cleaned ~/.tmpTripster on shell3 and app on 146.169.46.220 except node_modules'
 scp -q -r ~/Tripster-serverside/app dii14@shell3.doc.ic.ac.uk:.tmpTripster
 echo 'copied app from here to shell3:~/.tmpTripster'
 ssh -q dii14@shell3.doc.ic.ac.uk 'scp -q -r .tmpTripster/* dii14@146.169.46.220:~'
