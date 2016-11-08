a3132d32242280a7e1f8dbe44770b5d3d32bd618
dii14 <dii14@ic.ac.uk>
2016-11-08 00:35:59 +0000

    Update server.js

--- app/server.js
+++ app/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.get('/', function (req, res) {
-   res.send("This is working mtfs!!!");
+   res.send("This is working!!!");
 });
 
 app.get('/process_get', function(req, res) {
