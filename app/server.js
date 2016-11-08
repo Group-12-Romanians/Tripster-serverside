e0538567c91ea722981b7fc6c64e0a593a0173a6
Dragos Ilie <dii14@cloud-vm-45-211>
2016-11-08 00:00:35 +0000

    Second try changing deployment method

--- app/server.js
+++ app/server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.get('/index.html', function (req, res) {
+app.get('/', function (req, res) {
    res.send("This is working mtfs!!!");
 });
 
--- deploy_to_server.sh
+++ deploy_to_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scp -r Tripster-serverside dii14@shell3.doc.ic.ac.uk:.tmpTripster
+scp -r ~/Tripster-serverside/app dii14@shell3.doc.ic.ac.uk:.tmpTripster
 ssh dii14@shell3.doc.ic.ac.uk 'scp -r .tmpTripster/* dii14@146.169.46.220:~'
-ssh dii14@shell3.doc.ic.ac.uk 'ssh dii14@146.169.46.220 "'"cd Tripster-serverside && forever stopall && forever start server.js"'"'
+ssh dii14@shell3.doc.ic.ac.uk 'ssh dii14@146.169.46.220 "'"cd app && npm install && forever stopall && forever start server.js"'"'
 
