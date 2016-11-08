bc82f194333028759afe8c9f9b2b405da28f26fa
dii14 <dii14@ic.ac.uk>
2016-11-08 00:31:42 +0000

    Added execution info

--- deploy_to_server.sh
+++ deploy_to_server.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-ssh dii14@shell3.doc.ic.ac.uk 'rm -rf ~/.tmpTripster/*'
-scp -r ~/Tripster-serverside/app dii14@shell3.doc.ic.ac.uk:.tmpTripster
-ssh dii14@shell3.doc.ic.ac.uk 'scp -r .tmpTripster/* dii14@146.169.46.220:~'
-ssh dii14@shell3.doc.ic.ac.uk 'ssh dii14@146.169.46.220 "'"cd app && npm install && forever stopall && forever start server.js"'"'
-
+ssh -q dii14@shell3.doc.ic.ac.uk 'rm -rf ~/.tmpTripster/*'
+echo 'cleaned ~/.tmpTripster on shell3'
+scp -q -r ~/Tripster-serverside/app dii14@shell3.doc.ic.ac.uk:.tmpTripster
+echo 'copied app from here to shell3:~/.tmpTripster'
+ssh -q dii14@shell3.doc.ic.ac.uk 'scp -q -r .tmpTripster/* dii14@146.169.46.220:~'
+echo 'copied app from shell3:~/.tmpTripster to server:~'
+ssh -q dii14@shell3.doc.ic.ac.uk 'ssh -q dii14@146.169.46.220 "'"cd app && npm install && forever stopall && forever start server.js"'"'
+echo 'installed dependencies && restart server'
