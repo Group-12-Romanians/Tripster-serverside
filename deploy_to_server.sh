d6057d8e8ba8e356ae7a61900b1d96fa311124cf
dii14 <dii14@ic.ac.uk>
2016-11-08 00:09:02 +0000

    Update deploy_to_server.sh

--- deploy_to_server.sh
+++ deploy_to_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ssh dii14@shell3.doc.ic.ac.uk 'rm -rf ~/.tmpTripster/*'
 scp -r ~/Tripster-serverside/app dii14@shell3.doc.ic.ac.uk:.tmpTripster
 ssh dii14@shell3.doc.ic.ac.uk 'scp -r .tmpTripster/* dii14@146.169.46.220:~'
 ssh dii14@shell3.doc.ic.ac.uk 'ssh dii14@146.169.46.220 "'"cd app && npm install && forever stopall && forever start server.js"'"'
