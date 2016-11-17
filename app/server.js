51dcee351b298f9bf7eb1d378cd7e50606f7bae8
Cristian Chirac <cc5614@ic.ac.uk>
2016-11-17 22:32:29 +0000

    Implemented basic videos for trips

--- app/server.js
+++ app/server.js
@@ -1,22 +1,42 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var request = require('request');
 var url = require('url');
+var uuid = require('node-uuid');
+var spawn = require('threads').spawn;
 var path = require('path');
 var db = require('./database');
 var mongoose = require('mongoose');
 var multer = require('multer');
 var storage = multer.diskStorage({ 
-		destination: path.join(__dirname, '../uploads'),
-		filename: function (req, file, cb) {
-			var new_name = req.query.photo_id;
-			cb(null, new_name + '.jpg');
-		}	
-	})
+destination: path.join(__dirname, '../uploads'),
+filename: function (req, file, cb) {
+var new_name = req.query.photo_id;
+cb(null, new_name + '.jpg');
+}
+})
 
 var upload = multer( {storage: storage }).single('photo');
+var videoshow = require('videoshow');
+
+var video_options = {
+  fps: 25,
+  loop: 3, // seconds
+  transition: false,
+  transitionDuration: 1, // seconds
+  videoBitrate: 1024,
+  videoCodec: 'libx264',
+  size: '640x?',
+  audioBitrate: '128k',
+  audioChannels: 2,
+  format: 'mp4'
+};
+
 mongoose.Promise = global.Promise;
 var app = express();
+var GOOGLE_API_KEY = 'AIzaSyBEcADKicF0ZeIooOSbh12Vu7BVyDOIjik';
+
 app.use(express.static(path.join(__dirname, '../uploads')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
@@ -49,7 +69,7 @@ app.post('/new_trip', function(req, res) {
 	var locations = req.body.locations;
 	var lines = locations.split('\n');
 	var trip_info = lines[0].split(',');
-	var evnts = [];
+	var evnts = [], images = [], coords = [];
 	for(var i = 1; i < lines.length; ++i) {
 		var data = lines[i].split(',');
 		if (data.length > 2) {
@@ -58,27 +78,90 @@ app.post('/new_trip', function(req, res) {
 				lat: data[1],
 				lng: data[2]
 			};
+			coords.push(evnt.lat + ',' + evnt.lng);
 			if (data.length > 3) {
 				evnt.img_ids = data.slice(3, data.length);
+				var evnt_images = evnt.img_ids.map(function get_image(image_id) {
+					return path.join(__dirname, '../uploads', image_id + '.jpg');
+				});
+				images = images.concat(evnt_images);
 			}
 			evnts.push(evnt);
 		}
 	}
+	
+	var preview_img_name = uuid.v4();
+	var video_name = uuid.v4() + '.mp4';
 	var trip = new db.Trip({
 		trip_id: trip_info[0],
 		name: trip_info[1],
-		preview_img: "1234",
-		preview_video: "v.mp4",
+		preview_img: preview_img_name,
+		preview_video: video_name,
 		owner: user_id,
 		events: evnts
 	});
-	console.log(trip);
-	trip.save().then(function(doc) {
-		res.send(doc);
-	}).catch(function(err) {
-		console.log(err);
-		res.status(500).send("Could not save new trip:" + err);
+
+	res.send(trip);
+
+	var video_thread = spawn(function create_video(input, done) {
+		var fs = input.fs, request = input.request;
+		var coords_path = input.coords.map(function path_point(latlong) {
+							return '|' + latlong;
+						})
+					      .reduce(function compute_path(acc, point) {
+							return acc + point;
+						});   	
+		var url = 'https://maps.googleapis.com/maps/api/staticmap?key=' 
+				+ input.GOOGLE_API_KEY 
+				+ '&size=640x640&path=color:0x0000ff|weight:5' 
+				+ coords_path;
+	
+		var preview_image_path = path.join(__dirname, '../uploads', input.preview_img_name + '.jpg');
+                request(url).pipe(fs.createWriteStream(preview_image_path));
+		var video_images = [preview_image_path].concat(input.images);
+
+		var video_path = path.join(__dirname, '../uploads', input.video_name);
+		videoshow(video_images, video_options)
+		.save(video_path)
+		.on('start', function() {
+			console.log('Started writing video ' + input.video_name);
+		})
+		.on('error', function(err) {
+			console.error('Error while writing video ' + input.video_name, err);
+		})
+		.on('end', function() {
+			console.log('Finished writing video ' + input.video_name 
+					+ ', now adding to db.');
+		});
+
+		input.trip.save().then(function(doc) {
+			console.log('Successfully saved ' + doc);
+		}).catch(function(err) {
+			console.log(err);
+		});
+
+		done();
 	});
+
+	video_thread
+	.send({
+		fs: fs,
+		request: request,
+
+		GOOGLE_API_KEY: GOOGLE_API_KEY,
+
+		preview_img_name: preview_img_name,
+		video_name: video_name,
+		coords: coords,
+		images: images,
+		trip: trip
+	})
+	.on('message', function() {
+		video_thread.kill();
+	})
+	.on('error', function(err) {
+		console.error('Video writing thread error: ', err);
+	})	
 });
 
 app.get('/all_users', function(req, res) {
