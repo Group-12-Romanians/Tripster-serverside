5e66fe773c68f0b6d3e768d79f5834923cf652ae
Dragos Ilie <dii14@cloud-vm-45-211>
2016-12-23 15:03:15 +0000

    changed server for deployment

--- deploy_to_server.sh
+++ deploy_to_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh -q dii14@shell3.doc.ic.ac.uk 'ssh -q dii14@146.169.46.220 "'"cd Tripster-serverside && git pull origin master && cd app && npm install && forever stopall && forever start server.js"'"'
+ssh -q dii14@shell3.doc.ic.ac.uk 'ssh -q dii14@146.169.46.142 "'"cd Tripster-serverside && git pull origin master && cd app && npm install && forever stopall && forever start server.js"'"'
 echo 'Worked!!!'
